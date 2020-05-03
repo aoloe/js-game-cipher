aabedb78763d8cc2ec5916bbb4037d1243adb6af
ale rimoldi <ale@graphicslab.org>
2020-05-03 22:18:14 +0200

    add the missing js files

---
+++ js/app.js
@@ -0,0 +1,311 @@
+function get_basedir(url) {
+  return url.slice(-1) == '/' ? url.slice(0, -1) : url.split('/').slice(0,-1).join('/');
+}
+
+basedir = get_basedir(window.location.pathname);
+
+var Pusher_channel_factory = (function() {
+  var pusher_instance = null;
+  var channels = new Map();
+  return {
+    get_instance: function(channel_name) {
+      if (pusher_instance === null) {
+        Pusher.logToConsole = false;
+        pusher = new Pusher(
+          '1e754324fb908641cba9', {
+            cluster: 'eu',
+            forceTLS: true
+        });
+      }
+      if (!channels.has(channel_name)) {
+          channels.set(channel_name, pusher.subscribe(channel_name));
+      }
+      return channels.get(channel_name);
+    }
+  }
+})();
+
+// https://stackoverflow.com/a/2117523/5239250
+function uuidv4() {
+  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+  );
+}
+
+Character = function(c, i) {
+  this.c = c;
+  this.i = i;
+  this.hint = i === 0;
+  this.transform = null;
+}
+Character.in_clear = new Set([' ', ',', '!', '?', '.', '(', ')', ';', ':', '%', '/', '*', '"', '«', '»', '“', '”', '-']);
+// map of diacritics: â → [^, a]
+Character.diacritics = new Map([
+  ['à', ['`', 'a']], ['á', ['\'', 'a']],['â', ['^', 'a']], ['ä', ['"', 'a']],
+  ['è', ['`', 'e']], ['é', ['\'', 'e']],['ê', ['^', 'e']], ['ë', ['"', 'i']],
+  ['ì', ['`', 'i']], ['í', ['\'', 'i']],['î', ['^', 'i']], ['ï', ['"', 'i']],
+  ['ò', ['`', 'o']], ['ó', ['\'', 'o']],['ô', ['^', 'o']], ['ö', ['"', 'o']],
+  ['ù', ['`', 'u']], ['ú', ['\'', 'u']],['û', ['^', 'u']], ['ü', ['"', 'u']],
+  ['ç', [',', 'c']], ['ñ', ['~', 'n']],
+]);
+// reversed map of diactritics: ^ → a → â / ^ → o → ô / ...
+Character.letter_to_diacritics = new Map();
+for (let item of Character.diacritics) {
+  let l_t_d = Character.letter_to_diacritics.get(item[1][0]);
+  if (typeof(l_t_d) === 'undefined') {
+    Character.letter_to_diacritics.set(item[1][0], new Map([[item[1][1], item[0]]]));
+  } else {
+    l_t_d.set(item[1][1], item[0]);
+  }
+}
+Character.is_uppercase = function(c) {
+  return c === c.toUpperCase();
+}
+Character.is_clear = function(c) {
+    return Character.in_clear.has(c);
+}
+Sentence = function(sentence) {
+    this.characters = [];
+    this.transformations = [];
+
+    let characters = new Map();
+
+    let i = 1;
+    let hint = false;
+    let n_dashes = 0;
+    for (let j = 0; j < sentence.length; j++) {
+      let c = sentence[j];
+      if (c === '_') {
+        hint = !hint;
+        n_dashes++;
+        continue;
+      } else if (hint || Character.is_clear(c)) {
+        this.characters.push(new Character(c, 0));
+      } else {
+        let t = {};
+        if (Character.is_uppercase(c)) {
+          c = c.toLowerCase();
+          t.uppercase = true;
+        }
+        if (Character.diacritics.has(c)) {
+          let d = Character.diacritics.get(c);
+          c = d[1];
+          t.diacritics = d[0];
+        }
+        if (Object.keys(t).length > 0) {
+          this.transformations[j - n_dashes] = t;
+        }
+        if (!characters.has(c)) {
+          characters.set(c, new Character(null, i));
+          i++;
+        }
+        this.characters.push(characters.get(c));
+      }
+    }
+}
+Sentence.prototype.has = function(c) {
+  if (c.c === null || c.c === '') {
+    return false;
+  }
+  for (character of this.characters) {
+    if ( character.i > 0 && character.c === c.c && character.i !== c.i) {
+      return true;
+    }
+  }
+  return false;
+}
+
+const app = new Vue({
+  el: '#app',
+  data: {
+    languages: {'de': 'Deutsch', 'it': 'Italiano', 'en': 'English', 'fr': 'Français'},
+    list: null,
+    sentence: null,
+    cipher_id: null,
+    description: null,
+    description_visible: false,
+    editing: false,
+    share_key: null,
+  },
+  mounted() {
+    // this.sentence = new Sentence("“ä” igäh isch _voll_ blöd, weisch?");
+    if (localStorage.player_id) { 
+      this.player_id = localStorage.player_id;
+    } else {
+      this.player_id = uuidv4();
+      localStorage.setItem('player_id', this.player_id);
+    }
+    this.get_list();
+    if (localStorage.cipher_id) {
+      this.select(localStorage.cipher_id);
+    }
+  },
+  watch: {
+    /*
+    new_sentence: {
+      handler(val) {
+        this.sentence = new Sentence(val);
+      }
+    }
+    */
+  },
+  methods: {
+    get_list: function() {
+      axios
+        .get(basedir+'/api/', {
+          params: {
+            action: 'list'
+          }
+        })
+        .then(response => {
+          console.log('response.data');
+          this.list = response.data;
+        });
+    },
+    // there seems to be no garantee that the items in $refs are in the correct order: we cannot pos as the index
+    edit: function() {
+      console.log('hey');
+      this.editing = true;
+      this.sentence = '';
+    },
+    update: function(sentence) {
+      this.sentence = new Sentence(sentence);
+    },
+    select: function(id) {
+      this.description_visible = false;
+      if (id === null) {
+        this.cipher_id = null;
+        localStorage.removeItem('cipher_id');
+        this.sentence = null;
+        this.description = null;
+        this.share_key = null;
+        localStorage.removeItem('share_key');
+        this.editing = false;
+        return;
+      }
+      axios
+        .get(basedir+'/api/', {
+          params: {
+            action: 'get',
+            id: id,
+          }
+        })
+        .then(response => {
+          localStorage.setItem('cipher_id', id);
+          this.cipher_id = id;
+          this.title = response.data.title;
+          this.sentence = new Sentence(response.data.sentence);
+          this.description = response.data.description;
+          if (localStorage.share_key) {
+            this.share_key = localStorage.share_key;
+            this.activate_sharing();
+          }
+        });
+    },
+    activate_sharing: function() {
+      var pusher_channel = Pusher_channel_factory.get_instance('cipher');
+      pusher_channel.bind('typing', function(data) {
+        if (data.share_key === app.share_key) {
+          app.add_shared_character(data.c, data.i);
+        }
+      });
+    },
+    share: function() {
+      axios
+        .post(basedir+'/api/', {
+          action: 'share',
+          id: this.cipher_id
+        })
+        .then(response => {
+          this.share_key = response.data.share_key;
+          localStorage.setItem('share_key', this.share_key);
+        })
+    },
+    join_shared: function() {
+      axios
+        .post(basedir+'/api/', {
+          action: 'join_shared',
+          key: this.share_key
+        })
+        .then(response => {
+          this.select(response.data.id);
+          this.activate_sharing();
+        })
+    },
+    create: function(title, language, sentence, description) {
+      axios
+        .post(basedir+'/api/', {
+          action: 'create',
+          title: title,
+          language: language,
+          sentence: sentence,
+          description: description
+        })
+        .then(response => {
+            this.get_list();
+            this.player_id = response.data['player_id'];
+            this.game_id = response.data['game_id'];
+            this.sentence = null;
+            this.editing = false;
+        });
+    },
+    toggle_description: function() {
+      this.description_visible = !this.description_visible;
+    },
+    prev_focus: function(pos) {
+      let c = null;
+      for (character of this.$refs.characters) {
+        if (character.pos < pos && (c === null || c.pos < pos)) {
+          c = character
+        }
+      }
+      if (c !== null) {
+        c.$el.focus();
+      }
+    },
+    next_focus: function(pos) {
+      let c = null;
+      for (character of this.$refs.characters) {
+        if (character.pos > pos && (c === null || c.pos < pos)) {
+          c = character
+        }
+      }
+      if (c !== null) {
+        c.$el.focus();
+      }
+    },
+    add_character(character, old_c) {
+      if (Character.is_uppercase(character.c)) {
+        character.c = character.c.toLowerCase();
+      }
+      if (Character.diacritics.has(character.c)) {
+        character.c = Character.diacritics.get(character.c)[1];
+      }
+      if (this.sentence.has(character)) {
+        character.c = old_c;
+      } else if (this.share_key !== null) {
+        axios
+          .post(basedir+'/api/', {
+            action: 'typing',
+            share_key: this.share_key,
+            c: character.c,
+            i: character.i
+          })
+          .then(response => {
+            // TODO: if i'ts not ok, show a warning?
+            // console.log('ok', response.data);
+          });
+      }
+    },
+    add_shared_character(c, i) {
+      for (character of this.sentence.characters) {
+        if (character.i == i) {
+          if (character.c !== c) {
+            character.c = c;
+          }
+          break;
+        }
+      }
+    }
+  }
+});
---
+++ js/components.js
@@ -0,0 +1,111 @@
+Vue.component('cipher-list', {
+  template: `<div>
+    <h1>Ciphered citations</h1>
+    <ul>
+      <li v-for="item in list"><a href="#" v-on:click="select(item.cipher_id)">{{item.title}}</a> ({{item.author}})</li>
+    </ul>
+  </div>`,
+  props: {
+    list: Array
+  },
+  methods: {
+    edit: function() {
+      this.$emit('edit', '');
+    },
+    select: function(cipher_id) {
+      this.$emit('select', cipher_id);
+    }
+  }
+});
+
+Vue.component('character', {
+  template: `<input type="text" v-model="c" minlength="1" maxlength="1" :pos="pos" :ref="'cipher-i-'+pos.toString()" v-on:keydown="keyfilter" v-on:keyup="keymonitor">`,
+  props: {
+    character: Object,
+    pos: Number,
+    transformations: Array
+  },
+  computed: {
+    c: {
+      get() {
+        let c = this.character.c;
+        if (c === null || c === '') {
+          return '';
+        }
+        if (typeof this.transformations[this.pos] !== 'undefined') {
+          let t = this.transformations[this.pos];
+          if (t.hasOwnProperty('diacritics')) {
+            let l_d = Character.letter_to_diacritics.get(t.diacritics);
+            if (l_d.has(c)) {
+              c = l_d.get(c);
+            }
+          }
+          if (t.hasOwnProperty('uppercase')) {
+            c = c.toUpperCase();
+          }
+          return c;
+        } else {
+          return c;
+        }
+      },
+      set(val) {
+        let old_c = this.character.c;
+        this.character.c = val;
+        this.$emit('add_character', this.character, old_c);
+      }
+    }
+  },
+  methods: {
+    keyfilter: function(e) {
+       if (e.code === 'Space' || e.code === 'ArrowRight' || e.code === 'ArrowLeft') {
+         e.preventDefault();
+       }
+    },
+    keymonitor: function(e) {
+      if (e.key == ' ' || e.key == 'ArrowRight') {
+        let pos = Number(e.srcElement.attributes.pos.value);
+        this.$emit('next_focus', pos);
+      } else if (e.key == 'ArrowLeft') {
+        let pos = Number(e.srcElement.attributes.pos.value);
+        this.$emit('prev_focus', pos);
+      }
+    }
+  }
+});
+
+Vue.component('editor', {
+  template: `<div class="creation">
+    <input type="text" v-model="title" placeholder="Title" required>
+    <select v-model="language" required>
+      <option v-for="(value, key) in languages" v-bind:value="key">
+        {{value}}
+      </option>
+    </select><br>
+    <textarea v-model="sentence"></textarea required><br>
+    <input type="text" v-model="description" placeholder="Description"><br>
+    <input type="button" value="Add" :disabled="title === '' || language === null || sentence === ''" v-on:click="create" placeholder="Description">
+  </div>`,
+  props: {
+    languages: Array
+  },
+  data: function() {
+    return {
+      sentence: null,
+      title: null,
+      description: null,
+      language: null,
+    }
+  },
+  watch: {
+    sentence: {
+      handler(val) {
+        this.$emit('update', this.sentence);
+      }
+    }
+  },
+  methods: {
+    create: function() {
+      this.$emit('create', this.title, this.language, this.sentence, this.description);
+    }
+  }
+});
